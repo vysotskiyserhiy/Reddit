5b5ae15499743363ac2b9432b7fa2ee55ba6e3a6
Serge Vysotsky <vysotskiyserhiy@gmail.com>
2020-05-25 14:26:16 +0300

    Initial Commit

---
+++ Reddit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reddit
+//
+//  Created by Serge Vysotsky on 25.05.2020.
+//  Copyright © 2020 Serge Vysotsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
